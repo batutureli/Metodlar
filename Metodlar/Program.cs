0ce9decbc8fbaeae983540e413574228a33f8885
Batuhan T <batutureli@gmail.com>
2021-01-17 00:00:52 +0300

    16 Ocak

---
+++ Metodlar/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Metodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine("------------------------------");
+            }
+
+
+            Console.WriteLine("--------------Metotlar-----------------");
+            //
+            //
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut","Yeşil",12,5);
+            sepetManager.Ekle2("uçak", "kanatlı", 12,6);
+            sepetManager.Ekle2("karpuz", "diyarbakır", 12,49);
+
+
+
+        }
+    }
+}
+
+
+
