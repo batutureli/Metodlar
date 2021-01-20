3811fca3533b4fcfa3a46a820125d15037add6a3
Batuhan T <batutureli@gmail.com>
2021-01-20 23:49:23 +0300

    İyi günler

--- Koleksiyonlar/Program.cs
+++ Koleksiyonlar/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace Koleksiyonlar
 {
@@ -6,8 +8,18 @@ namespace Koleksiyonlar
     {
         static void Main(string[] args)
         {
-            string[] isimler = new string[] {"Engin","Murat","Kerem","Halil" };
-            Console.WriteLine(isimler[0]);
+            //string[] isimler = new string[] { "engin", "murat", "kerem", "halil" };
+            //console.writeline(isimler[0]);
+             
+
+            List<string> isimler2 = new List<string> { "Engin","Murat","kerem","Halil"};
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
 
         }
     }
